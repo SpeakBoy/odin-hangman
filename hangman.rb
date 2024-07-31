f04fb575061f69595e6d9cd7f4793383315d0d7c
SpeakBoy <speakman@mcmaster.ca>
2024-07-31 17:51:42 -0400

    Add save and load functionality

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,16 @@
+require "json"
+
 class Hangman
 
   WORD_ARR = File.read("google-10000-english-no-swears.txt").split
 
-  def initialize
-    @chosen_word = ""
-    @guessed_word = ""
-    @guessed_letters = []
-    @incorrect_guesses = 0
-    @rounds_completed = 0
-    @winner = nil
+  def initialize(chosen_word = "", guessed_word = "", guessed_letters = [], incorrect_guesses = 0, rounds_completed = 0, winner = nil)
+    @chosen_word = chosen_word
+    @guessed_word = guessed_word
+    @guessed_letters = guessed_letters
+    @incorrect_guesses = incorrect_guesses
+    @rounds_completed = rounds_completed
+    @winner = winner
   end
 
   def select_word
@@ -20,7 +22,7 @@ class Hangman
   def guess_letter
     selected_letter = nil
     while selected_letter.nil?
-      puts "\nPlease select a letter you have not chosen already!"
+      puts "\nPlease select a letter!"
       selected_letter = gets.chomp.upcase
       if @guessed_letters.include?(selected_letter) || selected_letter.length != 1 || selected_letter != selected_letter[/[a-zA-Z]+/]
         puts "\nInput invalid. Please try again."
@@ -76,11 +78,33 @@ class Hangman
     end
     return false
   end
+
+  def to_json
+    JSON.dump ({
+      :chosen_word => @chosen_word,
+      :guessed_word => @guessed_word,
+      :guessed_letters => @guessed_letters,
+      :incorrect_guesses => @incorrect_guesses,
+      :rounds_completed => @rounds_completed,
+      :winner => @winner
+    })
+  end
+
+  def from_json(string)
+    data = JSON.load string
+    Hangman.new(data["chosen_word"], data["guessed_word"], data["guessed_letters"], data["incorrect_guesses"], data["rounds_completed"], data["winner"])
+  end
 end
 
 def game_loop(game)
   game_finished = false
   while !game_finished
+    puts "\nIf you would like to save your game, please type 'save'. \nOtherwise type anything else to continue playing! \nNote: Saving will overwrite your previous save."
+    if gets.chomp.downcase == "save"
+      puts "\nInput accepted."
+      File.write "save.json", game.to_json
+      puts "\nYour game has now been saved."
+    end
     letter_guessed = game.guess_letter
     game.check_for_correct_guess(letter_guessed)
     game.construct_guessed_word
@@ -89,12 +113,16 @@ def game_loop(game)
   end
 end
 
-puts "Welcome to Hangman! Press any key to play!"
-if gets
-  game = Hangman.new
+puts (File.read "save.json").class
+puts "\nWelcome to Hangman! Press any key to play! \nOr if you would like to load your save, please type 'load'!"
+game = Hangman.new
+input = gets.chomp.downcase
+if input == "load"
+  game = game.from_json(File.read "save.json")
+else
   game.select_word
-  game.construct_guessed_word
-  game.display_round_results
-  game_loop(game)
 end
+game.construct_guessed_word
+game.display_round_results
+game_loop(game)
 
